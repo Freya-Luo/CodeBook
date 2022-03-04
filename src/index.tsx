53b4afbf631b26cb650c524d40a48974eb248743
Freya_Luo <y233luo@gmail.com>
2022-03-04 14:28:43 -0500

    add: transpiler for the browser (no file system)

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,49 @@
-import * as esbuild from 'esbuild-wasm';
-import  ReactDOM  from "react-dom";
-import { useState, useEffect } from "react";
+import * as esbuild from 'esbuild-wasm'
+import ReactDOM from 'react-dom'
+import { useState, useEffect, useRef } from 'react'
 
 const App = () => {
-    const [input, setInput] = useState("");
-    const [code, setCode] = useState("");
+    const [input, setInput] = useState('')
+    const [code, setCode] = useState('')
+    const ref = useRef<any>()
 
     // initialize the ESBuild
-    const execService =async () => {
-        const service = await esbuild.startService({
+    const execService = async () => {
+        // make this service to be available outside the execService function
+        ref.current = await esbuild.startService({
             worker: true,
-            wasmURL: '/esbuild.wasm'
+            wasmURL: '/esbuild.wasm',
         })
-
     }
 
-    useEffect(()=>{
-        execService();
-    }, []); // execute the service only 1 time
+    useEffect(() => {
+        execService()
+    }, []) // execute the service only 1 time
 
-    const onClick = () => {
-        console.log(input)
+    const onClick = async () => {
+        if (!ref.current) return
+        // get the transpiler from ESBuild
+        const transpiler = ref.current.transform
+        const res = await transpiler(input, {
+            loader: 'jsx',
+            target: 'es2015',
+        })
+        setCode(res.code)
+        console.log(res)
     }
 
-    return <div>
-        <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
+    return (
         <div>
-            <button onClick={onClick}>Submit</button>
-            <pre>{code}</pre>
+            <textarea
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+            ></textarea>
+            <div>
+                <button onClick={onClick}>Submit</button>
+                <pre>{code}</pre>
+            </div>
         </div>
-    </div>
+    )
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'))
