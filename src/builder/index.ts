cea89bf3ed48e89cc317db60f68a45d6a474703d
Freya_Luo <y233luo@gmail.com>
2022-03-10 16:26:37 -0500

    refactor: split Builder (ESBuild) component

---
+++ src/builder/index.ts
@@ -0,0 +1,39 @@
+import * as esbuild from 'esbuild-wasm'
+import { unpkgPathPlugin } from './plugins/unpkg-path'
+import { fetchFilePlugin } from './plugins/fetch-file'
+
+let service: esbuild.Service
+
+/**
+ * This function works as a builder (transpiles and bundles) of the input raw code.
+ * It pass the input code to the ESBuild service and generates the bundled code.
+ *
+ * @param inputCode the string literal of user input raw code
+ * @returns bundled code
+ */
+const Builder = async (inputCode: string) => {
+  if (!service) {
+    // initialize the ESBuild
+    service = await esbuild.startService({
+      worker: true,
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+    })
+  }
+
+  // get the builder (combined transpiler & bundler) from ESBuild
+  const builder = service.build
+  const res = await builder({
+    entryPoints: ['index.js'],
+    bundle: true,
+    write: false,
+    plugins: [unpkgPathPlugin(), fetchFilePlugin(inputCode)],
+    define: {
+      'process.env.NODE_ENV': '"production"', // to get the string 'production' not a var
+      global: 'window',
+    },
+  })
+  // return the transpiled and bundled code
+  return res.outputFiles[0].text
+}
+
+export default Builder
--- src/index.tsx
+++ src/index.tsx
@@ -1,56 +1,26 @@
 import 'bulmaswatch/slate/bulmaswatch.min.css'
-import * as esbuild from 'esbuild-wasm'
 import ReactDOM from 'react-dom'
-import { useState, useEffect, useRef } from 'react'
-import { unpkgPathPlugin } from './plugins/unpkg-path'
-import { fetchFilePlugin } from './plugins/fetch-file'
+import { useState } from 'react'
 import CodeEditor from './components/code-editor'
 import Preview from './components/preview'
+import Builder from './builder'
 
 const App = () => {
-  const [input, setInput] = useState('')
-  const [code, setCode] = useState('')
-  const serviceRef = useRef<any>()
-
-  // initialize the ESBuild
-  const execService = async () => {
-    // make this service to be available outside the execService function
-    serviceRef.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    })
-  }
-
-  useEffect(() => {
-    execService()
-  }, []) // execute the service only 1 time
+  const [inputCode, setInputCode] = useState('')
+  const [bundledCode, setBundledCode] = useState('')
 
   const onClick = async () => {
-    if (!serviceRef.current) return
-
-    // get the builder (combined transpiler & bundler) from ESBuild
-    const builder = serviceRef.current.build
-    const res = await builder({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchFilePlugin(input)],
-      define: {
-        'process.env.NODE_ENV': '"production"', // to get the string 'production' not a var
-        global: 'window',
-      },
-    })
-
-    setCode(res.outputFiles[0].text)
+    const code = await Builder(inputCode)
+    setBundledCode(code)
   }
 
   return (
     <div>
-      <CodeEditor initialValue='// write your code here' onChange={(value) => setInput(value)} />
+      <CodeEditor initialValue='// write your code here' onChange={(value) => setInputCode(value)} />
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <Preview code={code} />
+      <Preview code={bundledCode} />
     </div>
   )
 }
