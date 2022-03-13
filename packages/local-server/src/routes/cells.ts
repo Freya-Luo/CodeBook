bb5502e9f489bbcf5485ffdaa439749b1874f6f4
Freya_Luo <y233luo@gmail.com>
2022-03-13 14:50:11 -0400

    add: cell router for fetching & updating cell files

---
+++ packages/local-server/src/routes/cells.ts
@@ -0,0 +1,45 @@
+import express from 'express';
+import fs from 'fs/promises';
+import path from 'path';
+
+interface Cell {
+  id: string;
+  content: string;
+  cellype: 'CODE' | 'MARKDOWN';
+}
+
+export const cellRouter = (filename: string, dir: string) => {
+  const router = express.Router();
+  const filepath = path.join(dir, filename);
+
+  /**
+   * This router provides an API that gets the file and extract a list of cells out of it.
+   * If the file does not exist, create it and set up the default content;
+   * otherwise just directly parse the file.
+   */
+  router.get('/cells', async (request, response) => {
+    try {
+      const res = await fs.readFile(filepath, { encoding: 'utf-8' });
+
+      response.send(JSON.parse(res));
+    } catch (err: any) {
+      if (err.code !== 'ENOENT') {
+        console.log(err.message);
+      }
+    }
+  });
+
+  /**
+   * This router provides an API that writes any updates back to the files.
+   * If the file does not exist, it will be created automatically.
+   */
+  router.post('/cells', async (request, response) => {
+    const { cells }: { cells: Cell[] } = request.body;
+
+    await fs.writeFile(filepath, JSON.stringify(cells), 'utf-8');
+
+    response.send({ status: 'success' });
+  });
+
+  return router;
+};
